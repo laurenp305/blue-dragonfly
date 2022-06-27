b1a16a5b5df4f595df10f65b5ddc16492c5f2a44
laurenp305 <lamor1800@gmail.com>
2022-06-27 17:26:48 -0400

    added lowercase and uppercase letters for password

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
