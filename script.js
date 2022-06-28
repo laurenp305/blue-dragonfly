653af648c1c970e58a4a117e80dfb374f214a0e8
laurenp305 <lamor1800@gmail.com>
2022-06-27 20:25:31 -0400

    closed first function

--- script.js
+++ script.js
@@ -3,35 +3,18 @@
 //Define generate password 
 function generatePassword() {
 console.log("Hey! You clicked the button!")
-
+}
 // 1. Prompt the user for the password criteria
 let name=prompt("Please type a password");
 
-if (passwordLength && characters) {
-  console.log('You got it!')
-}
-
-var passwordLength = "128";
-
-function genPassword() {
-  var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-  var passwordLength = 12;
-  var password = "";
-
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random()
-  }
-}
-
 //  a. password length (between) 8 < 12
+
 
 //  b. lowercase, uppercase, numbers, special characters 
 // 2. Validate the input (what the user inputs needs to be valid)
 // 3. Generatep assword based on criteria
 
 // 4. Display generated password on the page 
-return "Generated password will go here!";
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
