efe86c9b378a58cdaa760082bdfdda41a62f56db
laurenp305 <lamor1800@gmail.com>
2022-06-30 22:29:33 -0400

    added arrays for numbers, symbols, and lower and uppercase letters

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ var userWantsSymbols= window.confirm("Would you like to have symbols in your pas
 var userWantsLowercase= window.confirm("Would you like to have lowercase letters in your password?")
 var userWantsUppercase= window.confirm("Would you like to have uppercase letters in your password?")
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
 }
 //{
 //   var passwordLength=prompt("How long do you want your password to be? Choose between 8 and 128 characters");
