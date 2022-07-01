fab1387bb718d2fe8033fb4b18760e9b91d579dd
laurenp305 <lamor1800@gmail.com>
2022-06-30 22:39:42 -0400

    added 'cart' variable to store the options the user selected

--- script.js
+++ script.js
@@ -23,8 +23,18 @@ var userWantsUppercase= window.confirm("Would you like to have uppercase letters
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var uppercase = []
 
+var optionsCart = []
+
+//This makes all lowercase letters uppercase 
+for (var i = 0; i < lowercase.length; i++) {
+  uppercase[i] = lowercase[i].toUpperCase()
+}
+
+if (userWantsNumbers === true) {
+
+}
 
 }
 //{
