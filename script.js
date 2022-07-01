3891f7fb348651f4e521f8c6cb35fb38aeb0dff8
laurenp305 <lamor1800@gmail.com>
2022-06-30 23:16:36 -0400

    fixed variable in line 9

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ if (!max) {
   max = min
   min = 0
 }
-
+var rand = Math.random() 
+  return Math.floor(min*(1 - rand) + rand*max)
 }
+
 
 function getRandomItem(list) {
   return list[randomInt(0, list.length - 1)]
